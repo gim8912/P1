e40bd424219d8fb5646d028edc662160388725c5
gim8912 <gim8912@gmail.com>
2014-04-13 01:11:13 +0900

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,27 @@ $(document).ready(function(){
 	var i=0;
 	var j=0;
 	////////////// 
+	for(i=0;i<6;i++){
+		Random_Val = Math.floor((Math.random()*6)+1);
+		for(j=0;j<i;j++){
+			if(card[j]==Random_Val){
+				Random_Val = Math.floor((Math.random()*6)+1);
+				j=0;
+			}
+		}
+		card1[i] = Random_Val;
+	}
+	for(i=0;i<6;i++){
+		Random_Val = Math.floor((Math.random()*6)+1);
 
+		for(j=0;j<i;j++){
+			if(card[j]==Random_Val){
+				Random_Val =Math.floor((Math.random()*6)+1);
+				j=0;
+			}
+		}
+		card2[i] = Random_Val;
+	}
 	//////////////
 
 	var Y = function(){
@@ -29,88 +49,113 @@ $(document).ready(function(){
 	$('.clear').click(function() {
 		location.reload();
 	});
-	$('.c1').click(function(){
-		$(this).toggleClass('k'+card1[0]);
-		savepoint[count] = card1[0];
+		$('.c1').click(function(){
+		$(this).toggleClass('k1');
+		savepoint[count] = 1;
 		savepoint2[count] = 1;
 		count +=1;
 		Y();
 	});
+
+
 	$('.c2').click(function(){
-		$(this).toggleClass('k'+card1[1]);
-		savepoint[count] = card1[1];
+		$(this).toggleClass('k3');
+		savepoint[count] = 3;
 		savepoint2[count] = 2;
 		count +=1;
 		Y();
 	});
+
+
 	$('.c3').click(function(){
-		$(this).toggleClass('k'+card1[2]);
-		savepoint[count] = card1[2];
+		$(this).toggleClass('k1');
+		savepoint[count] = 1;
 		savepoint2[count] = 3;
 		count +=1;
 		Y();
 	});
+
+
 	$('.c4').click(function(){
-		$(this).toggleClass('k'+card1[3]);
-		savepoint[count] = card1[3];
+		$(this).toggleClass('k4');
+		savepoint[count] = 4;
 		savepoint2[count] = 4;
 		count +=1;
 		Y();
 	});
+
+
 	$('.c5').click(function(){
-		$(this).toggleClass('k'+card1[4]);
-		savepoint[count] = card1[4];
+		$(this).toggleClass('k4');
+		savepoint[count] = 4;
 		savepoint2[count] = 5;
 		count +=1;
 		Y();
 	});
+
+
 	$('.c6').click(function(){
-		$(this).toggleClass('k'+card2[5]);
-		savepoint[count] = card1[5];
+		$(this).toggleClass('k6');
+		savepoint[count] = 6;
 		savepoint2[count] = 6;
 		count +=1;
 		Y();
 	});
+
+
 	$('.c7').click(function(){
-		$(this).toggleClass('k'+card2[0]);
-		savepoint[count] = card2[0];
+		$(this).toggleClass('k5');
+		savepoint[count] = 5;
 		savepoint2[count] = 7;
 		count +=1;
 		Y();
 	});
+
+
 	$('.c8').click(function(){
-		$(this).toggleClass('k'+card2[1]);
-		savepoint[count] = card2[1];
+		$(this).toggleClass('k2');
+		savepoint[count] = 2;
 		savepoint2[count] = 8;
 		count +=1;
 		Y();
 	});
+
+
 	$('.c9').click(function(){
-		$(this).toggleClass('k'+card2[2]);
-		savepoint[count] = card2[2];
+		$(this).toggleClass('k2');
+		savepoint[count] = 2;
 		savepoint2[count] = 9;
 		count +=1;
 		Y();
 	});
+
+
 	$('.c10').click(function(){
-		$(this).toggleClass('k'+card2[3]);
-		savepoint[count] = card2[3];
+		$(this).toggleClass('k3');
+		savepoint[count] = 3;
 		savepoint2[count] = 10;
 		count +=1;
 		Y();
 	});
+
+
 	$('.c11').click(function(){
-		$(this).toggleClass('k'+card2[4]);
-		savepoint[count] = card2[4];
+		$(this).toggleClass('k6');
+		savepoint[count] = 6;
 		savepoint2[count] = 11;
 		count +=1;
 		Y();
 	});
+
+
 	$('.c12').click(function(){
-		$(this).toggleClass('k'+card2[5]);
-		savepoint[count] = card2[5];
+		$(this).toggleClass('k5');
+		savepoint[count] = 5;
 		savepoint2[count] = 12;
 		count +=1;
 		Y();
 	});
+
+
 });
+
