c5501afaf92cb08296f0651ea41cfb2c3548b47b
gim8912 <gim8912@gmail.com>
2014-04-13 00:58:30 +0900

    Update script.js

--- script.js
+++ script.js
@@ -11,26 +11,7 @@ $(document).ready(function(){
 
 	
 	////////////// 
-	for(var i=0;i<6;i++){
-		Random_Val = Math.Random()*(6)+1;
-		for(var j=0;j<i;j++){
-			if(card[j]==Random_Val){
-				Random_Val = Math.Random()*(6)+1;
-				j=0;
-			}
-		}
-		card1[i] = Random_Val;
-	}
-	for(var i=0;i<6;i++){
-		Random_Val = Math.Random()*(6)+1;
-		for(var j=0;j<i;j++){
-			if(card[j]==Random_Val){
-				Random_Val = Math.Random()*(6)+1;
-				j=0;
-			}
-		}
-		card2[i] = Random_Val;
-	}
+
 	//////////////
 	
 	var Y = function(){
