3bb5b6df0e41a9a15b3b1d56f23e2c03c99f4916
gim8912 <gim8912@gmail.com>
2014-04-13 01:48:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 	var check = 0;
 	var savepoint = [0, 0];
 	var savepoint2 = [0, 0];
+	var lock = [0,0,0,0,0,0,0,0,0,0,0,0];
 	
 
 	var card1 = [0,0,0,0,0,0];
@@ -38,6 +39,8 @@ $(document).ready(function(){
 	var Y = function(){
 		if(count==2){
 			if(savepoint[0] == savepoint[1]){
+				lock[savepoint[0]-1] = 1;
+				lock[savepoint[1]-1] = 1;
 				checkpoint+=1;
 				count=0;
 			}
@@ -56,87 +59,111 @@ $(document).ready(function(){
 	
 	
 	$('.c1').click(function(){
-		$(this).addClass('k'+card1[0]);
-		savepoint[count] = card1[0];
-		savepoint2[count] = 1;
-		count +=1;
-		Y();
+		if(lock[0]==0){
+			$(this).addClass('k'+card1[0]);
+			savepoint[count] = card1[0];
+			savepoint2[count] = 1;
+			count +=1;
+			Y();
+		}
 	});
 	$('.c2').click(function(){
-		$(this).addClass('k'+card1[1]);
-		savepoint[count] = card1[1];
-		savepoint2[count] = 2;
-		count +=1;
-		Y();
+		if(lock[1]==0){
+			$(this).addClass('k'+card1[1]);
+			savepoint[count] = card1[1];
+			savepoint2[count] = 2;
+			count +=1;
+			Y();
+		}
 	});
 	$('.c3').click(function(){
-		$(this).addClass('k'+card1[2]);
-		savepoint[count] = card1[2];
-		savepoint2[count] = 3;
-		count +=1;
-		Y();
+		if(lock[2]==0){
+			$(this).addClass('k'+card1[2]);
+			savepoint[count] = card1[2];
+			savepoint2[count] = 3;
+			count +=1;
+			Y();
+		}
 	});
 	$('.c4').click(function(){
-		$(this).addClass('k'+card1[3]);
-		savepoint[count] = card1[3];
-		savepoint2[count] = 4;
-		count +=1;
-		Y();
+		if(lock[3]==0){
+			$(this).addClass('k'+card1[3]);
+			savepoint[count] = card1[3];
+			savepoint2[count] = 4;
+			count +=1;
+			Y();
+		}
 	});
 	$('.c5').click(function(){
-		$(this).addClass('k'+card1[4]);
-		savepoint[count] = card1[4];
-		savepoint2[count] = 5;
-		count +=1;
-		Y();
+		if(lock[4]==0){
+			$(this).addClass('k'+card1[4]);
+			savepoint[count] = card1[4];
+			savepoint2[count] = 5;
+			count +=1;
+			Y();
+		}
 	});
 	$('.c6').click(function(){
-		$(this).addClass('k'+card1[5]);
-		savepoint[count] = card1[5];
-		savepoint2[count] = 6;
-		count +=1;
-		Y();
+		if(lock[5]==0){
+			$(this).addClass('k'+card1[5]);
+			savepoint[count] = card1[5];
+			savepoint2[count] = 6;
+			count +=1;
+			Y();
+		}
 	});
 	$('.c7').click(function(){
-		$(this).addClass('k'+card2[0]);
-		savepoint[count] = card2[0];
-		savepoint2[count] = 7;
-		count +=1;
-		Y();
+		if(lock[6]==0){
+			$(this).addClass('k'+card2[0]);
+			savepoint[count] = card2[0];
+			savepoint2[count] = 7;
+			count +=1;
+			Y();
+		}
 	});
 	$('.c8').click(function(){
-		$(this).addClass('k'+card2[1]);
-		savepoint[count] = card2[1];
-		savepoint2[count] = 8;
-		count +=1;
-		Y();
+		if(lock[7]==0){
+			$(this).addClass('k'+card2[1]);
+			savepoint[count] = card2[1];
+			savepoint2[count] = 8;
+			count +=1;
+			Y();
+		}
 	});
 	$('.c9').click(function(){
-		$(this).addClass('k'+card2[2]);
-		savepoint[count] = card2[2];
-		savepoint2[count] = 9;
-		count +=1;
-		Y();
+		if(lock[8]==0){
+			$(this).addClass('k'+card2[2]);
+			savepoint[count] = card2[2];
+			savepoint2[count] = 9;
+			count +=1;
+			Y();
+		}
 	});
 	$('.c10').click(function(){
-		$(this).addClass('k'+card2[3]);
-		savepoint[count] = card2[3];
-		savepoint2[count] = 10;
-		count +=1;
-		Y();
+		if(lock[9]==0){
+			$(this).addClass('k'+card2[3]);
+			savepoint[count] = card2[3];
+			savepoint2[count] = 10;
+			count +=1;
+			Y();
+		}
 	});
 	$('.c11').click(function(){
-		$(this).addClass('k'+card2[4]);
-		savepoint[count] = card2[4];
-		savepoint2[count] = 11;
-		count +=1;
-		Y();
+		if(lock[10]==0){
+			$(this).addClass('k'+card2[4]);
+			savepoint[count] = card2[4];
+			savepoint2[count] = 11;
+			count +=1;
+			Y();
+		}
 	});
 	$('.c12').click(function(){
-		$(this).addClass('k'+card2[5]);
-		savepoint[count] = card2[5];
-		savepoint2[count] = 12;
-		count +=1;
-		Y();
+		if(lock[11]==0){
+			$(this).addClass('k'+card2[5]);
+			savepoint[count] = card2[5];
+			savepoint2[count] = 12;
+			count +=1;
+			Y();
+		}
 	});
 });
